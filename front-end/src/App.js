bb08cd620d9d72423284bec5bbf3a639c1467111
yassin mohamed <yassin_mohamed310@outlook.com>
2022-05-08 07:48:32 +0200

    Create Fills Structure

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
+import { About, Footer, Header, Skills, Testimonials, Work } from './container'
+import { Navbar } from './components'
+
 
 function App() {
   return (
-    <div className="App">
-
+    <div className="app">
+      <Navbar />
+      <Header />
+      <About />
+      <Work />
+      <Skills />
+      <Testimonials />
+      <Footer />
     </div>
   );
 }
